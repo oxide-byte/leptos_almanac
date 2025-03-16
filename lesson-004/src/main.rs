5546310e83709c5689a6aa0d75eebbf947bc2ddc
oxide-byte <oxidebyte@gmail.com>
2025-03-16 16:27:32 +0100

    Add Builder Dialect on Lesson 004

--- lesson-004/src/main.rs
+++ lesson-004/src/main.rs
@@ -1,6 +1,8 @@
+use leptos::ev;
 use leptos::ev::MouseEvent;
 use leptos::prelude::*;
 use leptos::logging::log;
+use leptos::html::{ button };
 
 #[component]
 pub fn CounterButton(counter: ReadSignal<i32>, on_click: impl FnMut(MouseEvent)+ 'static) -> impl IntoView {
@@ -14,6 +16,19 @@ pub fn CounterButton(counter: ReadSignal<i32>, on_click: impl FnMut(MouseEvent)+
 }
 
 #[component]
+pub fn CounterButtonBuilder(counter: ReadSignal<i32>, on_click: impl FnMut(MouseEvent)+ 'static) -> impl IntoView {
+    let button_class = "bg-green-700 hover:bg-green-800 px-20 py-3 text-white rounded-lg";
+
+    let button = button()
+        .class(button_class)
+        .on(ev::click, on_click)
+        .child(counter);
+
+    button.into_view()
+}
+
+
+#[component]
 pub fn App() -> impl IntoView {
     let (counter, set_counter) = signal(0);
     let on_click = move |_| {
@@ -21,10 +36,17 @@ pub fn App() -> impl IntoView {
         set_counter.update(|counter| *counter += 1);
     };
     view! {
+        <div>
         <CounterButton
             counter=counter
             on_click=on_click
         />
+        <br/>
+        <CounterButtonBuilder
+            counter=counter
+            on_click=on_click
+        />
+        </div>
     }
 }
 
